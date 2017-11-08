35e95654457af0d2bd62bd6a61f0e41ba25a1b11
Joe Cheeks <jcheeks@github.com>
2017-11-08 01:28:25 -0500

    Fix GuestUser

---
+++ UMW_Room_Scheduler/src/umw_room_scheduler/Room.java
@@ -0,0 +1,25 @@
+package umw_room_scheduler;
+/**
+*@Author Jazeb Raja
+*/
+
+public class Room {
+    private String roomNum;
+    private boolean avEquip;
+    private String roomBuilding;
+    
+    public Room(String num, String build, boolean av){
+        roomNum = num;
+        avEquip = av;
+        roomBuilding = build;
+    }
+    public String getRoomNum(){
+        return roomNum;
+    }
+    public boolean getAVEquip(){
+        return avEquip;
+    }
+    public String getRoomBuilding(){
+        return roomBuilding;
+    }
+}
---
+++ UMW_Room_Scheduler/src/umw_room_scheduler/User.java
@@ -0,0 +1,26 @@
+package umw_room_scheduler;
+
+
+/**
+ * @Author Aaron Bloomfield
+ */
+//User class
+public class User {
+    protected String UserName;
+    
+    public User(String name){
+        UserName = name;
+    }
+    
+    public String getUserName(){
+        return UserName;
+    }
+}
+
+
+
+//Registered user class
+
+//Admin user class
+
+
