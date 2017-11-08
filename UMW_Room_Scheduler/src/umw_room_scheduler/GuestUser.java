cd0fc83e72130b42d9f0953a22d6be3ad06e29b2
Cheeks <Joe@192.168.1.7>
2017-11-08 01:15:51 -0500

    Fix GuestUser

---
+++ UMW_Room_Scheduler/src/umw_room_scheduler/GuestUser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umw_room_scheduler;
+
+/**
+ *
+ * @Author Aaron Bloomfield
+ */
+//GuestUser class
+public class GuestUser extends User{
+    protected String GuestUserName;
+    
+    public GuestUser(String name){
+        super (name);
+    }
+}
