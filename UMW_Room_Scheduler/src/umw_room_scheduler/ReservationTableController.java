a3ccd77fa45b4b8b53edc256c8b30b100660c9fb
Joe Cheeks <jcheeks@github.com>
2017-12-01 12:58:36 -0500

    Addedfxml and reservation

---
+++ UMW_Room_Scheduler/src/umw_room_scheduler/ReservationTableController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umw_room_scheduler;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Joe
+ */
+public class ReservationTableController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    //Configure what table does
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
