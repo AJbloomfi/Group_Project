0042258674209dc542fd10559991d10fe75a953d
Jazeb R <Jazeb R@10.35.119.95>
2017-12-02 15:09:23 -0500

    Added function to create a new User form a guest user

--- UMW_Room_Scheduler/src/umw_room_scheduler/GuestUser.java
+++ UMW_Room_Scheduler/src/umw_room_scheduler/GuestUser.java
@@ -5,18 +5,33 @@
  */
 package umw_room_scheduler;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  * 
  * @Author Aaron Bloomfield
  */
 //GuestUser class
 public class GuestUser extends User{
-    protected String GuestUserName;
     
-    public GuestUser(String name){
-        super (name);
+    
+    public GuestUser(){
     }
-    public void BecomeUser(String name, String BannerID){
-        
+    public void BecomeUser(){
+        PrintWriter fw = null;
+        try{
+            fw = new PrintWriter("registeredusers.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            System.out.println("What would you like you UserName to be?");
+            Scanner input = new Scanner(System.in);
+            String username = input.next();
+            bw.write(username);
+            fw.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
